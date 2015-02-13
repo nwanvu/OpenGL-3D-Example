91e29d7f117d18889b10cedf20d9f55bf7424a78
nwanvu <ap.beginmyfuture@gmail.com>
2015-02-13 10:21:05 +0700

    first commit

---
+++ app/src/main/java/com/nwanvu/opengl3dexample/MyGLRenderer.java
@@ -0,0 +1,121 @@
+package com.nwanvu.opengl3dexample;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import com.nwanvu.opengl3dexample.obj.Cube;
+import com.nwanvu.opengl3dexample.obj.Square;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+
+/**
+ * OpenGL Custom renderer used with GLSurfaceView
+ */
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+	Context context; // Application's context
+
+	private Square square;
+	private Cube cube;
+	private Cube cube1;
+	private Cube cube2;
+
+	private static float angleCube = 0;
+	private static float speedCube = -1.5f;
+
+	// Constructor with global application context
+	public MyGLRenderer(Context context) {
+		this.context = context;
+
+		square = new Square();
+
+		cube = new Cube();
+		cube1 = new Cube();
+		cube2 = new Cube();
+	}
+
+	// Call back when the surface is first created or re-created
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Set color's clear-value
+		// to
+		// // black
+		// gl.glClearDepthf(1.0f); // Set depth's clear-value to farthest
+		// gl.glEnable(GL10.GL_DEPTH_TEST); // Enables depth-buffer for hidden
+		// // surface removal
+		// gl.glDepthFunc(GL10.GL_LEQUAL); // The type of depth testing to do
+		// gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST); //
+		// nice
+		// // perspective
+		// // view
+		// gl.glShadeModel(GL10.GL_SMOOTH); // Enable smooth shading of color
+		// gl.glDisable(GL10.GL_DITHER); // Disable dithering for better
+		// // performance
+
+		// You OpenGL|ES initialization code here
+		// ......
+
+		gl.glDisable(GL10.GL_DITHER);
+		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
+		gl.glClearColor(0, 0, 0, 0);
+		gl.glEnable(GL10.GL_CULL_FACE);
+		gl.glShadeModel(GL10.GL_SMOOTH);
+		gl.glEnable(GL10.GL_DEPTH_TEST);
+	}
+
+	// Call back after onSurfaceCreated() or whenever the window's size changes
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		if (height == 0)
+			height = 1; // To prevent divide by zero
+		float aspect = (float) width / height;
+
+		// Set the viewport (display area) to cover the entire window
+		gl.glViewport(0, 0, width, height);
+
+		// Setup perspective projection, with aspect ratio matches viewport
+		gl.glMatrixMode(GL10.GL_PROJECTION); // Select projection matrix
+		gl.glLoadIdentity(); // Reset projection matrix
+		// Use perspective projection
+		GLU.gluPerspective(gl, 45, aspect, 0.1f, 100.f);
+
+		gl.glMatrixMode(GL10.GL_MODELVIEW); // Select model-view matrix
+		gl.glLoadIdentity(); // Reset
+
+	}
+
+	// Call back to draw the current frame.
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		// Clear color and depth buffers using clear-value set earlier
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+		gl.glLoadIdentity();
+		gl.glTranslatef(0.0f, 0.0f, -6.0f);
+		//square.draw(gl);
+
+		// ----- Render the Color Cube -----
+		gl.glLoadIdentity(); // Reset the model-view matrix
+		gl.glTranslatef(-2.5f, 0.0f, -20.0f); // Translate right and into the
+												// screen
+		gl.glRotatef(angleCube, 1.0f, 0f, 0f); // rotate about the axis (1,1,1)
+												// (NEW)
+		cube.draw(gl); // Draw the cube (NEW)
+		// Update the rotational angle after each refresh (NEW)
+		angleCube += speedCube; // (NEW)
+
+		// / cube2
+		gl.glLoadIdentity(); // Reset the model-view matrix
+		gl.glTranslatef(0.0f, 0.0f, -20.0f);
+		gl.glRotatef(angleCube * 2, 1.0f, 0f, 0f);
+		cube1.draw(gl);
+
+		// / cube3
+		gl.glLoadIdentity(); // Reset the model-view matrix
+		gl.glTranslatef(2.5f, 0.0f, -20.0f);
+		gl.glRotatef(angleCube * 4, 1.0f, 0f, 0f);
+		cube2.draw(gl);
+
+	}
+}
---
+++ app/src/main/java/com/nwanvu/opengl3dexample/obj/Cube.java
@@ -0,0 +1,99 @@
+package com.nwanvu.opengl3dexample.obj;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import javax.microedition.khronos.opengles.GL10;
+/*
+ * Define the vertices for a representative face.
+ * Render the cube by translating and rotating the face.
+ */
+public class Cube {
+   private FloatBuffer vertexBuffer; // Buffer for vertex-array
+  
+   private float[] vertices = { // Vertices for a face at z=0
+      -1.0f, -1.0f, 0.0f,  // 0. left-bottom-front
+       1.0f, -1.0f, 0.0f,  // 1. right-bottom-front
+      -1.0f,  1.0f, 0.0f,  // 2. left-top-front
+       1.0f,  1.0f, 0.0f   // 3. right-top-front
+   };
+  
+   private float[][] colors = {  // Colors of the 6 faces
+      {1.0f, 0.5f, 0.0f, 1.0f},  // 0. orange
+      {1.0f, 0.0f, 1.0f, 1.0f},  // 1. violet
+      {0.0f, 1.0f, 0.0f, 1.0f},  // 2. green
+      {0.0f, 0.0f, 1.0f, 1.0f},  // 3. blue
+      {1.0f, 0.0f, 0.0f, 1.0f},  // 4. red
+      {1.0f, 1.0f, 0.0f, 1.0f}   // 5. yellow
+   };
+  
+   // Constructor - Set up the buffers
+   public Cube() {
+      // Setup vertex-array buffer. Vertices in float. An float has 4 bytes
+      ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+      vbb.order(ByteOrder.nativeOrder()); // Use native byte order
+      vertexBuffer = vbb.asFloatBuffer(); // Convert from byte to float
+      vertexBuffer.put(vertices);         // Copy data into buffer
+      vertexBuffer.position(0);           // Rewind
+   }
+   
+   // Draw the shape
+   public void draw(GL10 gl) {
+      gl.glFrontFace(GL10.GL_CCW);    // Front face in counter-clockwise orientation
+      gl.glEnable(GL10.GL_CULL_FACE); // Enable cull face
+      gl.glCullFace(GL10.GL_BACK);    // Cull the back face (don't display)
+  
+      gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+      gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+      
+      // front
+      gl.glPushMatrix();
+      gl.glTranslatef(0.0f, 0.0f, 1.0f);
+      gl.glColor4f(colors[0][0], colors[0][1], colors[0][2], colors[0][3]);
+      gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+      gl.glPopMatrix();
+  
+      // left
+      gl.glPushMatrix();
+      gl.glRotatef(270.0f, 0.0f, 1.0f, 0.0f);
+      gl.glTranslatef(0.0f, 0.0f, 1.0f);
+      gl.glColor4f(colors[1][0], colors[1][1], colors[1][2], colors[1][3]);
+      gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+      gl.glPopMatrix();
+  
+      // back
+      gl.glPushMatrix();
+      gl.glRotatef(180.0f, 0.0f, 1.0f, 0.0f);
+      gl.glTranslatef(0.0f, 0.0f, 1.0f);
+      gl.glColor4f(colors[2][0], colors[2][1], colors[2][2], colors[2][3]);
+      gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+      gl.glPopMatrix();
+  
+      // right
+      gl.glPushMatrix();
+      gl.glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
+      gl.glTranslatef(0.0f, 0.0f, 1.0f);
+      gl.glColor4f(colors[3][0], colors[3][1], colors[3][2], colors[3][3]);
+      gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+      gl.glPopMatrix();
+ 
+      // top
+      gl.glPushMatrix();
+      gl.glRotatef(270.0f, 1.0f, 0.0f, 0.0f);
+      gl.glTranslatef(0.0f, 0.0f, 1.0f);
+      gl.glColor4f(colors[4][0], colors[4][1], colors[4][2], colors[4][3]);
+      gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+      gl.glPopMatrix();
+ 
+      // bottom
+      gl.glPushMatrix();
+      gl.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+      gl.glTranslatef(0.0f, 0.0f, 1.0f);
+      gl.glColor4f(colors[5][0], colors[5][1], colors[5][2], colors[5][3]);
+      gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
+      gl.glPopMatrix();
+  
+      gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+      gl.glDisable(GL10.GL_CULL_FACE);
+   }
+}
